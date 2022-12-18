59c77ea86cae4b1e3ec053689798b5132afb9ff3
blackSooooo <blacksooooo@naver.com>
2022-12-18 19:33:49 +0900

    feat: EnableAspectJAutoProxy 어노테이션 추가

--- src/main/kotlin/blacksooooo/aop/AopApplication.kt
+++ src/main/kotlin/blacksooooo/aop/AopApplication.kt
@@ -2,8 +2,10 @@ package blacksooooo.aop
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.EnableAspectJAutoProxy
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 class AopApplication
 
 fun main(args: Array<String>) {
