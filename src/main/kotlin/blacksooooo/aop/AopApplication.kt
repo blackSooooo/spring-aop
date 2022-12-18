249658fda162e233fe5ee3a80b63b37fbec65391
blackSooooo <blacksooooo@naver.com>
2022-12-18 18:40:39 +0900

    initial setting

---
+++ src/main/kotlin/blacksooooo/aop/AopApplication.kt
@@ -0,0 +1,11 @@
+package blacksooooo.aop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AopApplication
+
+fun main(args: Array<String>) {
+	runApplication<AopApplication>(*args)
+}
---
+++ src/test/kotlin/blacksooooo/aop/AopApplicationTests.kt
@@ -0,0 +1,13 @@
+package blacksooooo.aop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AopApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
